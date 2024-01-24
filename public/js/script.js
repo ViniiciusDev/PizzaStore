599e5f32bf7371566ce99b3756675af418c4325b
Vinicius Gomes <ViniiciusGomes27@gmail.com>
2024-01-24 21:17:49 +0100

    Modal Complete

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,5 @@
+let modalQt = 1;
+
 const selector = (el) => document.querySelector(el);
 const selectorAll = (el) => document.querySelectorAll(el);
 
@@ -5,10 +7,38 @@ const selectorAll = (el) => document.querySelectorAll(el);
 pizzaJson.map((item, index) => {
     let pizzaItem = selector('.models .pizza-item').cloneNode(true);
     /* Informazioni in pizzaItem */
+
+    pizzaItem.setAttribute('data-key', index);
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `$ ${item.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    pizzaItem.querySelector('a').addEventListener('click', (e) => {
+        // Prevent the default action of reload the page when is clicked
+        e.preventDefault();
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        modalQt = 1;
+
+        selector('.pizzaBig img').src = pizzaJson[key].img;
+        selector('.pizzaInfo h2').innerHTML = pizzaJson[key].name;
+        selector('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        selector('.pizzaInfo--actualPrice').innerHTML = `$ ${pizzaJson[key].price.toFixed(2)}`;
+        selector('.pizzaInfo--size.selected').classList.remove('selected');
+        selectorAll('.pizzaInfo--size').forEach((size, sizeIndex) => {
+            if(sizeIndex == 2) {
+                size.classList.add('selected');
+            }
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+        });
+
+        selector('.pizzaInfo--qt').innerHTML = modalQt;
+
+        selector('.pizzaWindowArea').style.opacity = 0;
+        selector('.pizzaWindowArea').style.display = 'flex';
+        setTimeout(() => {
+            selector('.pizzaWindowArea').style.opacity = 1;
+        }, 200)
+    });
 
     selector('.pizza-area').append( pizzaItem );
-})
+});
